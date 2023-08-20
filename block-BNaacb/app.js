5ff491a6267537c50946d5be79ae2b544f1a8d76
chirag gaur <chiragwalker@gmail.com>
2023-08-20 11:29:18 +0530

    done

--- block-BNaacb/app.js
+++ block-BNaacb/app.js
@@ -1,5 +1,10 @@
 let express = require("express");
 let app = express();
+let mongoose = require("mongoose");
+
+// moongose connect
+
+mongoose.connect("mongodb://127.0.0.1:27017/myarticle");
 
 app.listen(2000, () => {
   console.log("server is live");
