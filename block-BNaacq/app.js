cfb129e5826d709e2edf623afcf14404127a6611
chirag gaur <chiragwalker@gmail.com>
2023-08-21 16:41:28 +0530

    done

--- block-BNaacq/app.js
+++ block-BNaacq/app.js
@@ -1,9 +1,12 @@
 let express = require("express");
 let mongoose = require("mongoose");
+let Users = require("./models/users");
+let Articles = require("./models/articles");
+let Comments = require("./models/comments");
 
 // connect mongoose to db
 
-mongoose.connect("mongodb://127.0.0.1:27017/blog");
+mongoose.connect("mongodb://127.0.0.1:27017/vlog");
 
 // initiate express server
 
@@ -11,7 +14,16 @@ let app = express();
 
 //middleware
 
+app.use(express.json());
+
 // routes
+
+app.post(`/articles`, (req, res) => {
+  //   const userId = req.params.userId;
+  const data = req.body;
+  Articles.create(data);
+  console.log(data);
+});
 
 //errors
 
